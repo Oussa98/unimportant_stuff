521f730a74245e5e583046e46c69349593e15f9e
Your-Tipical-Friend <54190181+Your-Tipical-Friend@users.noreply.github.com>
2022-05-20 17:04:35 +0100

    Add files via upload
    I found a website on the dark net (the onion router) and it was interesting with mathematical stuff so I thought it was about ancient science, especially when they put the X card from Hunter X Hunter which I thought was cool. but I recognized the language it was written in (the Tifinagh, used in North Africa) and made this thing to translate it. they were asking for bitcoin.

---
+++ amazigh_alpha_translate_to_latin_alpha - Copie.py
@@ -0,0 +1,135 @@
+"""
+
+"""
+dic = {
+    'ⴰ':'a',
+    'ⴱ':'b',
+    'ⴲ':'ḇ',
+    'ⴳ':'g',
+    'ⴴ':'go',
+    'ⴵ':'dj',
+    'ⴶ':'dj',
+    'ⴷ':'d',
+    'ⴸ':'ḏ',
+    'ⴹ':'the',
+    'ⴺ':'ḍ̱',
+    'ⴻ':'e',
+    'ⴼ':'of',
+    'ⴽ':'k',
+    'ⴾ':'k',
+    'ⴿ':'ḵ',
+    'ⵀ':'h',
+    'ⵁ':'h',
+    'ⵂ':'h',
+    'ⵃ':'ḥ',
+    'ⵄ':'ʕ',
+    'ⵅ':'kh',
+    'ⵆ':'kh',
+    'ⵇ':'q',
+    'ⵈ':'q',
+    'ⵉ':'i',
+    'ⵊ':'j',
+    'ⵋ':'j',
+    'ⵌ':'ẓ',
+    'ⵍ':'l',
+    'ⵎ':'m',
+    'ⵏ':'n',
+    'ⵐ':'ny',
+    'ⵑ':'ng',
+    'ⵒ':'p',
+    'ⵓ':'u',
+    'ⵔ':'r',
+    'ⵕ':'ṛ',
+    'ⵖ':'ɣ',
+    'ⵗ':'ɣ',
+    'ⵘ':'ɣ',
+    'ⵙ':'s',
+    'ⵚ':'ṣ',
+    'ⵛ':'sh',
+    'ⵜ':'t',
+    'ⵝ':'t',
+    'ⵞ':'ch',
+    'ⵟ':'ṭ',
+    'ⵠ':'v',
+    'ⵡ':'w',
+    'ⵢ':'y',
+    'ⵣ':'z',
+    'ⵤ':'z',
+    'ⵥ':'ẓ',
+    '<':'e',
+    '--':'o',
+    'ⵯ':'ʷ',
+    ' ':'     ',
+    '\\':'\\',
+    '\n':'\n'
+    }
+
+
+
+
+
+
+c = ''' '''
+text = '''
+
+ⵉⵏ ⴹ ⵏⴰⵢⵎ ⴼ ⴴⴷ ⵡⵎ ⴹⴻⵔ ⵉⵣ ⵏⵯ ⴳⴷ ⵝⵙⴻⵒⵝ ⵁⴻⵎ
+ⴹ ⵎⴰⵝⵙⵉⵎⵓⵎ ⵉⵣ ⴴⴷ ⴹ ⵍⵔⴷ ⴼ ⴻⵠⵔⵝⴻⵏ
+ⵡⵎ ⴹⵔ ⵉⵣ ⵏⵯ ⴳⴷ ⵝⵙⴻⵒⵝ ⵁⴻⵎ
+ⴹ ⵔⵓⵍⴻⵔ ⴰⵝ ⴹⴷⴰⵢ ⴼ ⴷⴻⵏ
+ⴰⵏ\\ⵉⵝⵢⵯ ⵡⴻ ⴰⵔ ⵒⵔⴰⵢⴻⵏ
+ⵏⴷ ⴰⵏ\\ⵉ ⵝ ⵢⵯ ⵡⴻ ⴰⵔ ⴽⴰⵍⴻⵏ
+ⴴⵉⴷ ⵓⵙ ⵡⴹ ⴹ ⵒⴰ ⵟ ⴼ ⵙⵝⵔⴰⵢⵝⵏⴻⵏ
+ⴹ ⵒⴰⵟ ⴹⴰⵟ ⴱⵢ ⵢⵔ ⴴⵔⴰⵙ ⵢⵯ ⴴⵢⵠⴻⵏ
+ⵏⵝ ⵝ ⴹⵡⵣ ⵢⵯ ⴹⴰⵎⵏ ⴹⴻⵎ
+ⵏⵔ ⴹ ⵍⵙⵝ ⵎⴻⵏ
+ⴰⵎⴻⵏ
+
+ⵒⴻⴰⵙ
+ⵉⵏ ⴹ ⵏⴰⵢⵎ ⴼ ⴴⴷ ⵡⵎ ⴹⴻⵔ ⵉⵣⵏⵯ ⴳⴷ ⵝⵙⴻⵒⵝ ⵁⴻⵎ
+ⵏ\\ⵎ
+ⵉⵣ ⵡⵉⴹ ⵏⵯ ⵙⵡⵙⵒⵛⴻⵏ
+ⴹ ⴳⵢⴷ ⵎⵔⵢⵝⴻⵏ
+ⵝ ⴹⵡⵣ ⵡ ⴰⵔ ⴰⵡⴰⵔ ⴼ ⵁⴻⵡ
+ⴹⵡⵣ ⵡ ⴱⵍⵉⵠ ⵉⵏ ⴹ ⵓⵏⵙⴻⵏ
+ⵒⴻⵔⴼⵔⵎ ⴹ ⵒⵔⴰⵢⴻⵏ
+ⵏⴷ ⴼⵔⵎ ⴹⵢⵔ ⵡⴻⵢⵟ ⴹⵢ ⴴⵢⵠⴻⵏ
+ⴹⵡⵣ ⵡ ⴱⵍⵉⵠ ⵉⵏ ⴹ ⵔⴻⵠⴻⵍⴰⵛⴻⵏ Eⴰⵝ ⵡⴰⵣ ⴷⵙⴻⵏ
+ⵝ ⵢⵯ ⵏⴷ ⵢo ⴰⵏⵝⵉⵙⵉⴷⴻⵏ
+ⵏⴷ ⵉⵏ ⴹ ⴻⵏⴷ ⴹⵢ ⴰⵔ ⵙⵔⵝⴻⵏ
+ⴹⵢ ⴰⵔ ⴹⵡⵣ ⵡ ⴰⵔ ⵓⵒⴻⵏ ⴴⵢⴷⴰⵏⵙ ⴼⵔⵎ ⵁⴻⵎ
+ⴹⵢ ⴰⵔ ⴹⵡⵣ ⵡ ⵡⴻ ⴽⴰⵍ ⴹ ⵡⵏⴻⵏ ⵎⴻⵏ
+'''
+
+for line in text:
+    print(dic[line], end="")
+
+
+
+
+
+
+#this is the result
+    '''
+in the naym of god wm theer iz neʷ gd tsept hem
+the matsimum iz god the lrd of evrten
+wm ther iz nʷ gd tsept hem
+the ruler at theday of den
+an\ityʷ we ar prayen
+nd an\i t yʷ we ar kalen
+goid us wthe the paṭ of straytnen
+the paṭ theaṭ by yr goras yʷ goyven
+nt  t  thewz  yʷ  theamn  theem
+nr  the  lst  men
+amen
+
+peas
+in  the  naym  of  god  wm  theer  iznʷ  gd  tsept  hem
+n\m
+iz  withe  nʷ  swspshen
+the  gyd  mryten
+t  thewz  w  ar  awar  of  hew
+thewz  w  bliv  in  the  unsen
+perofrm  the  prayen
+nd  ofrm  theyr  weyṭ  they  goyven
+thewz  w  bliv  in  the  revelashen
+    '''
